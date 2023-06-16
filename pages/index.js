27f90e132477b068593851bc47f215e065fbafee
gulnar98 <115141861+gulnar98@users.noreply.github.com>
2023-06-16 11:35:00 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import 'normalize.css/normalize.css'
 import Hero from "../components/Hero"
 import Navigation from "../components/Navigation"
+import Head from 'next/head'
 
 import styles from './asset/css/style.module.css'
 
@@ -9,7 +10,14 @@ import styles from './asset/css/style.module.css'
 function HomePage() {
     return <>
 
+        <Head>
 
+                            <script 
+            	data-appid="648c1e3a462e896372371249" 
+            	src="https://cdn.app.insightify.xyz/v1/648c1e3a462e896372371249/record.js">
+            </script>
+
+         </Head>
        <div className={styles.wrapper}>
        <div className={styles.nav}> <Navigation/></div>
         <div className={styles.hero}><Hero/></div>
